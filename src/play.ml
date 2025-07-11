cb7bdbf3e0194e72aecbb69cede90f7c40f1f073
kuviman <kuviman@gmail.com>
2025-07-12 02:09:47 +0400

    haha

--- src/play.ml
+++ src/play.ml
@@ -17,6 +17,15 @@ module All_in_one = struct
         | _ -> Error)
 end
 
+module Abstract = struct
+  module type Interpreter = sig
+    type value
+    type expr
+
+    val eval : expr -> value
+  end
+end
+
 module Error = struct
   module Value = struct
     type t = unit
@@ -25,73 +34,122 @@ module Error = struct
   end
 end
 
-module Const = struct
-  module Expr = struct
-    type 'v t = { value : 'v }
+module Parts = struct
+  module Const = struct
+    module Expr = struct
+      type 'v t = { value : 'v }
 
-    let eval : 'v t -> 'v = fun { value } -> value
-  end
-end
-
-module Add = struct
-  module type ValueS = sig
-    type t
-
-    val add : t -> t -> t
+      let eval : 'v t -> 'v = fun { value } -> value
+    end
   end
 
-  module Expr = struct
-    type 'e t = {
-      lhs : 'e;
-      rhs : 'e;
-    }
+  module Int = struct
+    module Value = struct
+      type t = int
 
-    let eval : 'e 'v. eval:('e -> 'v) -> add:('v -> 'v -> 'v) -> 'e t -> 'v =
-     fun ~eval ~add { lhs; rhs } -> add (eval lhs) (eval rhs)
+      let add : t -> t -> t = ( + )
+    end
   end
-end
 
-module Int = struct
-  module Value = struct
-    type t = int
+  module Add = struct
+    type addable =
+      | Error of Error.Value.t
+      | Int of Int.Value.t
 
-    let add : t -> t -> t = ( + )
+    let add : addable -> addable -> addable =
+     fun a b ->
+      match (a, b) with
+      | Error _, _ | _, Error _ -> Error (Error.Value.make ())
+      | Int a, Int b -> Int (a + b)
+
+    module type ValueS = sig
+      type t
+
+      val into_addable : t -> addable
+      val from_addable : addable -> t
+    end
+
+    module type S = functor (I : Abstract.Interpreter) -> sig
+      module Expr : sig
+        type t
+      end
+
+      val eval : Expr.t -> I.value
+    end
+
+    module Make (I : Abstract.Interpreter) (V : ValueS with type t = I.value) =
+    struct
+      module Expr = struct
+        type t = {
+          lhs : I.expr;
+          rhs : I.expr;
+        }
+      end
+
+      let eval : Expr.t -> I.value =
+       fun { lhs; rhs } ->
+        add (I.eval lhs |> V.into_addable) (I.eval rhs |> V.into_addable)
+        |> V.from_addable
+    end
   end
-end
 
-module String = struct
-  module Value = struct
-    type t = string
+  module String = struct
+    module Value = struct
+      type t = string
 
-    let add : t -> t -> t = ( ^ )
+      let add : t -> t -> t = ( ^ )
+    end
   end
 end
 
 module Combined = struct
-  type _unused = unit
+  module rec Unused : sig end = struct end
 
-  and expr =
-    | E_Const of value Const.Expr.t
-    | E_Add of expr Add.Expr.t
+  and Value : sig
+    type t
 
-  and value =
-    | V_Int of Int.Value.t
-    | V_String of String.Value.t
-    | V_Error of Error.Value.t
+    include Parts.Add.ValueS with type t := t
+  end = struct
+    type t =
+      | Int of Parts.Int.Value.t
+      | String of Parts.String.Value.t
+      | Error of Error.Value.t
 
-  let rec _unused = ()
+    let into_addable (value : t) : Parts.Add.addable =
+      let fail () : Parts.Add.addable = Error (Error.Value.make ()) in
+      match value with
+      | Int x -> Int x
+      | Error x -> Error x
+      | String _ -> fail ()
 
-  and eval : expr -> value = function
-    | E_Const expr -> Const.Expr.eval expr
-    | E_Add expr -> Add.Expr.eval ~eval ~add expr
+    let from_addable (value : Parts.Add.addable) : t =
+      match value with
+      | Int x -> Int x
+      | Error x -> Error x
+  end
 
-  and add : value -> value -> value =
-   fun a b ->
-    let fail () = V_Error (Error.Value.make ()) in
-    match (a, b) with
-    | V_Error _, _ | _, V_Error _ -> V_Error (Error.Value.make ())
-    | V_Int a, V_Int b -> V_Int (Int.Value.add a b)
-    | V_Int _, _ -> fail ()
-    | V_String a, V_String b -> V_String (String.Value.add a b)
-    | V_String _, _ -> fail ()
+  and Interpreter : (Abstract.Interpreter with type value = Value.t) = struct
+    type _unused = unit
+
+    and expr =
+      | E_Const of value Parts.Const.Expr.t
+      | E_Add of Add.Expr.t
+
+    and value = Value.t
+
+    let rec _unused = ()
+
+    and eval : expr -> value = function
+      | E_Const expr -> Parts.Const.Expr.eval expr
+      | E_Add expr -> Add.eval expr
+  end
+
+  and Add : sig
+    module Expr : sig
+      type t
+    end
+
+    val eval : Expr.t -> Value.t
+  end =
+    Parts.Add.Make (Interpreter) (Value)
 end
